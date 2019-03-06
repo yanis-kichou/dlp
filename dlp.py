4d2eca8944186caaaa00b973342ab73deaab8059
KICHOU <3703169@ppti-14-401-16.ufr-info-p6.jussieu.fr>
2019-03-06 19:39:25 +0100

    avanecement dlp

--- dlp.py
+++ dlp.py
@@ -5,33 +5,52 @@ import random
 
 #Exercice 1
 #Q1
+def PGCD(a,b):
+    while b!=0:
+        r=a%b
+        a,b=b,r
+    return a
 def bezout(a, b):
-    return
-
+    if a==0 and b==0 : return (0,0,0)
+    if b==0: return (a//abs(a),0,abs(a))
+    (u,v,p)=bezout(b,a%b)
+    return (v,(u-v*(a//b)),p)
 
 #Q2
 def inv_mod(a, n):
-    return
+    for i in range(1,n):
+        if(((i*a) % n) == 1):
+            return i
+    return n
 
 
 def invertibles(N):
-    return
+    inv=list()
+    for i in range(N):
+        if(inv_mod(i,N)!=N):
+            inv.append(i)
+    return inv
 
 
 #Q3
 def phi(N):
-    return
+    inversible=invertibles(N)
+    phiN=0
+    for i in inversible:
+        if(PGCD(i,N)==1 and i<N):
+            phiN+=1
+    return phiN
 
 
 #Exercice 2
 #Q1
 def exp(a, n, p):
-    return
+    return math.exp(a,p)%n
 
 
 #Q2
 def factor(n):
-    return
+   return
 
 
 #Q3
